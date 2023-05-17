03215cfd415f1b87f3b807aeda42fdce9d844eb7
awetprog <awetokubagaber.tewelde@miu.edu>
2023-05-17 17:52:33 -0500

    fix

--- SimpleCalculatorLab/calculatormod.js
+++ SimpleCalculatorLab/calculatormod.js
@@ -22,9 +22,7 @@ exports.displays=function(req,res,vals){
     res.write("<p style=\"background-color:orange\" ; style=\"font:14pt bold\";>The result is: ");
           res.write(String(result));
     res.write("</p>");
-    res.write(
-        "<a href='https://awetprog.github.io/SimpleCalculatorLab/index.html'>Another calculation</a>"
-      );
+    res.write( "<a href='https://awetprog.github.io/SimpleCalculatorLab/index.html'>Another calculation</a>" );
     res.write("</body>");
     res.write("</html>");
     return res.end();
